ca32dda7f15bc9085e35e1f2faa8b3174a5dfaa2
tishajune06 <jindaltisha14@gmail.com>
2026-07-26 13:19:53 +0530

    Initial webhook receiver setup

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("PR Guardian is alive");
+});
+app.post("/webhook", (req, res) => {
+
+    console.log("Event:", req.headers['x-github-event']);
+
+    console.log("Action:", req.body.action);
+
+    console.log("PR Number:", req.body.pull_request.number);
+
+    console.log("Repository:", req.body.repository.name);
+    console.log(
+    "User:",
+    req.body.pull_request?.user?.login
+); 
+
+    res.send("Webhook processed");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
